5e840dd170fde79ab6615e6b9abf301d1f613411
Chike Chris <cchike2017@gmail.com>
2019-07-16 18:51:19 -0400

    updated app.css file

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   return (
-    <div className="container">
+    <div className="app-Container">
       <Logo />
       <Display />
 
